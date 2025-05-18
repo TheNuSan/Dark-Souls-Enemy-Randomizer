0e1b3beb534d71d15d3a6495cd56c59c87256c48
Nusan <lezanu@gmail.com>
2025-05-19 00:02:46 +0200

    Add build bat file + randomizer data

---
+++ build.bat
@@ -0,0 +1,5 @@
+pyinstaller randomizer.spec
+copy "dist\randomizer.exe" "E:\Jeux\Steam\steamapps\common\Dark Souls Prepare to Die Edition\DATA"
+cd "E:\Jeux\Steam\steamapps\common\Dark Souls Prepare to Die Edition\DATA"
+randomizer.exe
+pause
